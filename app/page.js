554f0f65ea0919ea8ace1748a2f6e0f4b9b06309
Madison Ebersole <madison.ebersole@radai.com>
2024-11-12 22:53:05 -0500

    start adding gradient

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <h2>hello world</h2>
-      <p>ready to start?</p>  
+      <p>ready to make some gains?</p>  
     </main>
   );
 }
